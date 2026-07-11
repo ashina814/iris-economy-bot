930bcbc1ef6385983b6a8d2d0f99f58191afcb20
ashina814 <tajitajifj0133@gmail.com>
2026-07-11 15:16:46 +0900

    Load 4am daily reset extension

--- src/discord-entry-operations.js
+++ src/discord-entry-operations.js
@@ -4,8 +4,10 @@ const {
   installDiscordOperations,
   installEconomyExtensions
 } = require("./operations-extension");
+const { installDailyFourAmReset } = require("./daily-reset-4am");
 
 installEconomyExtensions();
+installDailyFourAmReset();
 installDiscordOperations();
 
 require("./discord-entry-manual-join.js");
